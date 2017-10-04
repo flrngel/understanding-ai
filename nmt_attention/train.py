b41459662b1c044368a926cb40bf2829ef105fe3
flrngel <flrngel@gmail.com>
2017-10-04 14:39:28 +0900

    add attention mechanism

--- nmt_attention/train.py
+++ nmt_attention/train.py
@@ -6,11 +6,11 @@ from util import *
 batch_size = 32
 pad_length = 60
 embed_vector_size = 100
-rnn_hidden_size = 100
+rnn_hidden_size = 128
 
 # init
 global_step = tf.Variable(0, trainable=False, name='global_step')
-learning_rate = tf.train.exponential_decay(0.1, global_step, 500, 0.95, staircase=True)
+learning_rate = tf.train.exponential_decay(0.01, global_step, 500, 0.95, staircase=True)
 
 # Load classes
 src_table = tf.contrib.lookup.index_table_from_file('./iwslt15/vocab.en', default_value=0)
@@ -28,11 +28,11 @@ src_dataset = tf.contrib.data.TextLineDataset(['./iwslt15/train.en'])
 tgt_dataset = tf.contrib.data.TextLineDataset(['./iwslt15/train.vi'])
 
 # Convert data to word indices
-src_dataset = src_dataset.map(lambda string: tf.string_split([string]).values)
+src_dataset = src_dataset.map(lambda string: tf.concat([['<s>'], tf.string_split([string]).values, ['</s>']], 0))
 src_dataset = src_dataset.map(lambda words: (words, tf.size(words)))
 src_dataset = src_dataset.map(lambda words, size: (src_table.lookup(words), size))
 
-tgt_dataset = tgt_dataset.map(lambda string: tf.string_split([string]).values)
+tgt_dataset = tgt_dataset.map(lambda string: tf.concat([['<s>'], tf.string_split([string]).values, ['</s>']], 0))
 tgt_dataset = tgt_dataset.map(lambda words: (words, tf.size(words)))
 tgt_dataset = tgt_dataset.map(lambda words, size: (tgt_table.lookup(words), size))
 
@@ -46,7 +46,7 @@ batched_dataset = dataset.padded_batch(batch_size,
 batched_iterator = batched_dataset.make_initializable_iterator()
 ((source, source_lengths), (target, target_lengths)) = batched_iterator.get_next()
 
-# Load embedding
+# Load embedding (dic limits to 100000)
 src_embed = tf.Variable(tf.random_normal([100000, embed_vector_size], stddev=0.1))
 tgt_embed = tf.Variable(tf.random_normal([100000, embed_vector_size], stddev=0.1))
 
@@ -60,9 +60,15 @@ projection_layer = layers_core.Dense(tgt_table_size)
 encoder_cell = tf.nn.rnn_cell.BasicLSTMCell(rnn_hidden_size)
 encoder_outputs, encoder_state = tf.nn.dynamic_rnn(encoder_cell, src_lookup, sequence_length=source_lengths, dtype=tf.float32)
 
+## Basic NTM
 decoder_cell = tf.nn.rnn_cell.BasicLSTMCell(rnn_hidden_size)
+## Attention NTM (Wrapping)
+attention_mechanism = tf.contrib.seq2seq.LuongAttention(rnn_hidden_size, encoder_outputs, memory_sequence_length=source_lengths)
+decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention_mechanism, attention_layer_size=rnn_hidden_size)
+attention_state = decoder_cell.zero_state(batch_size=tf.size(source_lengths), dtype=tf.float32)
+
 decode_helper = tf.contrib.seq2seq.TrainingHelper(tgt_lookup, target_lengths)
-decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, decode_helper, encoder_state, output_layer=projection_layer)
+decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, decode_helper, attention_state, output_layer=projection_layer)
 outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(decoder)
 logits = outputs.rnn_output
 
@@ -74,13 +80,22 @@ loss = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate)
 opt = optimizer.minimize(loss, global_step=global_step)
 
+# Training Section
+saver = tf.train.Saver()
+
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
   sess.run(tf.tables_initializer())
   sess.run(batched_iterator.initializer)
 
+  epoch = 0
   for i in range(100000):
     try:
-      print(sess.run([opt, loss, global_step]))
+      _, _loss, _global_step = sess.run([opt, loss, global_step])
+      print([epoch, _loss, _global_step])
     except tf.errors.OutOfRangeError:
       sess.run(batched_iterator.initializer)
+      epoch += 1
+      if epoch == 12:
+        save_path = saver.save(sess, "model.ckpt")
+        break
